1bf4b1de473e9928b5e6ab28294e4d8fe37e7819
inbaltako <inbaltako@gmail.com>
2016-05-06 15:19:25 +0300

    complete ui
    complete ui - fragment not included. basic algorithm set up.

---
+++ app/src/main/java/com/inbaltako/tictactoe/BoardActivity.java
@@ -0,0 +1,40 @@
+package com.inbaltako.tictactoe;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class BoardActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+    ReturnDialog returnDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+        returnDialog = new ReturnDialog();
+
+        // setting portrait mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // enable return button
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setElevation(0);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                returnDialog.show(getFragmentManager(), "returnToMenu");
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/inbaltako/tictactoe/ReturnDialog.java
@@ -0,0 +1,43 @@
+package com.inbaltako.tictactoe;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class ReturnDialog extends DialogFragment {
+
+    Button stay;
+    Button leave;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.return_dialog, null);
+        stay = (Button) view.findViewById(R.id.dialogButtonStay);
+        stay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        leave = (Button) view.findViewById(R.id.dialogButtonLeave);
+        leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MenuActivity.class));
+                getActivity().finish();
+            }
+        });
+
+        builder.setView(view);
+        builder.setCancelable(false);
+
+        return builder.create();
+    }
+}
