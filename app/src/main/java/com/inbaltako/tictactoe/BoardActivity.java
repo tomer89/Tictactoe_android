18e7ff95b48e348886294a019d5148b706a49edf
inbaltako <inbaltako@gmail.com>
2016-05-06 16:39:05 +0300

    add fragment

--- app/src/main/java/com/inbaltako/tictactoe/BoardActivity.java
+++ app/src/main/java/com/inbaltako/tictactoe/BoardActivity.java
@@ -1,22 +1,36 @@
 package com.inbaltako.tictactoe;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class BoardActivity extends AppCompatActivity {
 
     ActionBar actionBar;
     ReturnDialog returnDialog;
+    ImageButton btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
 
+        final Intent intent = new Intent(this, ScoreActivity.class);
+
         returnDialog = new ReturnDialog();
+        btn = (ImageButton) findViewById(R.id.boardBtn9);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+                finish();
+            }
+        });
 
         // setting portrait mode
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
---
+++ app/src/main/java/com/inbaltako/tictactoe/TieFragment.java
@@ -0,0 +1,43 @@
+package com.inbaltako.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+public class TieFragment extends Fragment {
+
+    ImageButton newGame;
+    ImageButton exit;
+
+    public TieFragment newInstance() {
+        return new TieFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tie_fragment, container, false);
+        newGame = (ImageButton) view.findViewById(R.id.newGameTie);
+        exit = (ImageButton) view.findViewById(R.id.exitBtnTie);
+
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MenuActivity.class));
+                getActivity().finish();
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+}
